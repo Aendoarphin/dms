e7e10a45e5ecdf914261b5b745e6494d9cdc000d
Arhon <aronp2000@gmail.com>
2025-08-10 17:46:17 -0500

    add hook to get current admin

---
+++ client/src/hooks/useGetAdmin.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import supabase from "@/util/supabase";
+
+export default function useGetAdmin(currentUser: { user: { id: string } }) {
+	const [adminId, setAdminId] = useState<string | null>(null);
+
+	useEffect(() => {
+		const getAdmin = async () => {
+			const { data, error } = await supabase.from("administrators").select("*");
+
+			if (error) {
+				console.error("Error fetching admins:", error);
+				return [];
+			}
+
+			if (data) {
+				setAdminId(data.find((admin) => admin.user_id === currentUser.user.id).user_id);
+			}
+		};
+
+		getAdmin();
+	}, [currentUser.user.id]);
+	return adminId;
+}
