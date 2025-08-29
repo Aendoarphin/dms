2d05f52746cab24ff4f7687338fcb93e225f13cb
Arhon Pineda <apineda@wsfcu.com>
2025-08-29 17:11:22 -0500

    add check to extend user session

--- client/src/hooks/useListenAuth.ts
+++ client/src/hooks/useListenAuth.ts
@@ -1,29 +1,36 @@
 import supabase from "@/util/supabase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 export default function useListenAuth(signedInTime: string) {
   const navigate = useNavigate();
+  const [lastActivityTime, setLastActivityTime] = useState(signedInTime);
 
   useEffect(() => {
     const checkSessionExpiration = async () => {
       try {
         const response = await fetch(import.meta.env.VITE_DEV_SERVER_URL);
         const currentTime = new Date().getTime();
-        const signedInTimestamp = new Date(signedInTime).getTime();
-        const timeDiff = (currentTime - signedInTimestamp) / 1000 / 60;
+        const signedInTimestamp = new Date(lastActivityTime).getTime();
         // minutes from time of sign-in
+        const timeDiff = (currentTime - signedInTimestamp) / 1000 / 60;
         if (
+          // User was inactive beyond session time
           timeDiff > import.meta.env.VITE_SESSION_DURATION &&
           localStorage.getItem(import.meta.env.VITE_COOKIE) &&
           response.ok
         ) {
           supabase.auth.signOut();
           localStorage.clear();
-          window.alert("Your session has expired. Please log in again.");
+          window.alert(
+            "Your session has expired due to inactivity. Please log in again."
+          );
           navigate("/login", { replace: true });
         } else if (!response.ok) {
           window.alert("Could not connect to server. Please try again later.");
+        } else {
+          // User wes active within session time
+          setLastActivityTime(new Date().toISOString());
         }
       } catch (error) {
         window.location.reload();
@@ -40,5 +47,5 @@ export default function useListenAuth(signedInTime: string) {
     return () => {
       document.removeEventListener("click", handleDocumentClick);
     };
-  }, [signedInTime, navigate]);
+  }, [signedInTime, navigate, lastActivityTime]);
 }
